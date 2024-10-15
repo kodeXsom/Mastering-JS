aef8b1c62527f06d29ef3c0db47b34e4bf0518f5
Somanath Naik <163634524+kodeXsom@users.noreply.github.com>
2024-10-15 14:55:02 +0000

    dataTypes done

---
+++ basic_01/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+// alert("kodeX") // we are using node js, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+    let name = "kodeX"
+    let age = 22
+    let isLoggedIn = false
+    let state;
+    let dist = null
+
+    //number => 2 to the power of 53
+    //bigInt 
+    //string => ""
+    //boolean =>true/false
+    //null => standalone value
+    //undefined => value not assigned
+    //symbol =>uniqueness
+
+
+
+
+    //object
+
+    console.log(typeof null); //object
+    console.log(typeof undefined); //undefined
+    
+    
