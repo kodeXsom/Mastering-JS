642c9ade7f8c44300eb3d458237b24bc5a673dcb
Somanath Naik <163634524+kodeXsom@users.noreply.github.com>
2024-10-16 03:08:18 +0000

    conversion of datatypes

---
+++ basic_01/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = undefined
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+
+let valuseInNumber = Number(score)
+
+console.log(typeof valuseInNumber);
+
+console.log(valuseInNumber);
+
+
+//"33"  => 33
+//"33abc" => NaN
+//true =>1; false => 0
+
+
+let isLoggedIn = "kodeX"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0=> false
+//"" => false
+//"kodeX" => true
+
+
+let someNumber = 33
+
+let stringNumber =String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
