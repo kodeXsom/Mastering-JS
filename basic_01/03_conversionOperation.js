23093e1ddf54f5f287e1fe38478b81c9208a68d9
Somanath Naik <163634524+kodeXsom@users.noreply.github.com>
2024-10-17 13:14:20 +0000

    conversion and operation done

--- basic_01/03_conversionOperation.js
+++ basic_01/03_conversionOperation.js
@@ -1,15 +1,15 @@
 let score = undefined
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 
 let valuseInNumber = Number(score)
 
-console.log(typeof valuseInNumber);
+// console.log(typeof valuseInNumber);
 
-console.log(valuseInNumber);
+// console.log(valuseInNumber);
 
 
 //"33"  => 33
@@ -20,7 +20,7 @@ console.log(valuseInNumber);
 let isLoggedIn = "kodeX"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true; 0=> false
@@ -32,6 +32,48 @@ let someNumber = 33
 
 let stringNumber =String(someNumber)
 
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// **************** Operations  *************** //
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "heyy"
+let str2 = " kodeX"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2 +2);
+// console.log(1+2+"2");
+// JS read the value from the beggingin and decide the type and then do the operation acc. to it!!!
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let  num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+// console.log(num1);
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
