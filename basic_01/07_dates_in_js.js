4b63ac390a8b283e03f41e6c153a62ef410415ff
Somanath Naik <163634524+kodeXsom@users.noreply.github.com>
2024-10-24 05:59:25 +0000

    re-read this

--- basic_01/07_dates_in_js.js
+++ basic_01/07_dates_in_js.js
@@ -17,3 +17,14 @@ console.log(myCreateedDate.toDateString());
 
 let myDate1 = new Date (2023,0,23, 5, 3)
 console.log(myDate1.toLocaleString());
+
+
+let myCreateedDate1 = new Date("2023-01-14") //yy-mm-dd
+let myCreateedDate2 = new Date("01-14-2023") //mm-dd-yy
+console.log(myCreateedDate1.toLocaleString());
+console.log(myCreateedDate2.toLocaleString());
+
+
+
+let myTimeStam =  Date.now()
+console.log(myTimeStam);
